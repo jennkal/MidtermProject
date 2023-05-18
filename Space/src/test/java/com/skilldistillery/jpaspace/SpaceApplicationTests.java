d66a8e1b6c0e8c26a9c2ac94c83cd52ee3d47603
jennkal <jennkalkowski@gmail.com>
2023-05-18 13:27:09 -0400

    Create Spring Boot MVC

---
+++ Space/src/test/java/com/skilldistillery/jpaspace/SpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpaspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
