ad5ada115d30c5127569f7d9bc1c92772d467dc5
krisvesely <kristine.vesely@gmail.com>
2023-05-19 15:33:04 -0400

    database schema complete and test inserts for each table added

--- DB/space.sql
+++ DB/space.sql
@@ -24,53 +24,26 @@ CREATE TABLE IF NOT EXISTS `user` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
-  `enabled` TINYINT NULL,
+  `enabled` TINYINT NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NULL,
   `role` VARCHAR(45) NULL,
+  `image_url` VARCHAR(2000) NULL,
+  `about` TEXT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `username_UNIQUE` (`username` ASC))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `comment`
+-- Table `classification`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `comment` ;
+DROP TABLE IF EXISTS `classification` ;
 
-CREATE TABLE IF NOT EXISTS `comment` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `User_id` INT NOT NULL,
-  `time_entered` DATETIME NOT NULL,
-  `title` VARCHAR(45) NOT NULL,
-  `body` TEXT(20000) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Comment_User_idx` (`User_id` ASC),
-  CONSTRAINT `fk_Comment_User`
-    FOREIGN KEY (`User_id`)
-    REFERENCES `user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `encounter`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `encounter` ;
-
-CREATE TABLE IF NOT EXISTS `encounter` (
+CREATE TABLE IF NOT EXISTS `classification` (
   `id` INT NOT NULL,
-  `User_id` INT NOT NULL,
-  `time_entered` DATETIME NOT NULL,
-  `capture_method` VARCHAR(45) NOT NULL,
-  `user_location` VARCHAR(100) NULL,
-  `body` TEXT(20000) NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_Encounter_User1_idx` (`User_id` ASC),
-  CONSTRAINT `fk_Encounter_User1`
-    FOREIGN KEY (`User_id`)
-    REFERENCES `user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  `name` VARCHAR(250) NOT NULL,
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
@@ -81,9 +54,15 @@ DROP TABLE IF EXISTS `category` ;
 
 CREATE TABLE IF NOT EXISTS `category` (
   `id` INT NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(250) NOT NULL,
+  `classification_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC))
+  INDEX `fk_category_classification1_idx` (`classification_id` ASC),
+  CONSTRAINT `fk_category_classification1`
+    FOREIGN KEY (`classification_id`)
+    REFERENCES `classification` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -93,13 +72,25 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `celestial_body` ;
 
 CREATE TABLE IF NOT EXISTS `celestial_body` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(250) NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `enabled` TINYINT NOT NULL,
+  `updated_at` DATETIME NULL,
+  `description` TEXT NULL,
+  `image_url` VARCHAR(2000) NULL,
+  `tracking_url` VARCHAR(2000) NULL,
   `category_id` INT NOT NULL,
-  `description` TEXT(200000) NULL,
+  `parent_celestial_body_id` INT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_celestial_body_category1_idx` (`category_id` ASC),
   UNIQUE INDEX `name_UNIQUE` (`name` ASC),
+  INDEX `fk_celestial_body_celestial_body1_idx` (`parent_celestial_body_id` ASC),
+  INDEX `fk_celestial_body_category1_idx` (`category_id` ASC),
+  CONSTRAINT `fk_celestial_body_celestial_body1`
+    FOREIGN KEY (`parent_celestial_body_id`)
+    REFERENCES `celestial_body` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `fk_celestial_body_category1`
     FOREIGN KEY (`category_id`)
     REFERENCES `category` (`id`)
@@ -109,66 +100,119 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `image`
+-- Table `celestial_body_comment`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `image` ;
+DROP TABLE IF EXISTS `celestial_body_comment` ;
 
-CREATE TABLE IF NOT EXISTS `image` (
+CREATE TABLE IF NOT EXISTS `celestial_body_comment` (
   `id` INT NOT NULL AUTO_INCREMENT,
+  `body` TEXT NOT NULL,
+  `enabled` TINYINT NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NULL,
+  `user_id` INT NOT NULL,
   `celestial_body_id` INT NOT NULL,
-  `link` VARCHAR(20000) NOT NULL,
-  `capture_method` VARCHAR(45) NULL,
+  `in_reply_to_id` INT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_image_celestial_body1_idx` (`celestial_body_id` ASC),
-  CONSTRAINT `fk_image_celestial_body1`
-    FOREIGN KEY (`celestial_body_id`)
-    REFERENCES `celestial_body` (`id`)
+  INDEX `fk_Comment_User_idx` (`user_id` ASC),
+  INDEX `fk_comment_celestial_body1_idx` (`celestial_body_id` ASC),
+  INDEX `fk_celestial_body_comment_celestial_body_comment1_idx` (`in_reply_to_id` ASC),
+  CONSTRAINT `fk_Comment_User`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `celestial_body_has_comment`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `celestial_body_has_comment` ;
-
-CREATE TABLE IF NOT EXISTS `celestial_body_has_comment` (
-  `celestial_body_id` INT NOT NULL,
-  `comment_id` INT NOT NULL,
-  PRIMARY KEY (`celestial_body_id`, `comment_id`),
-  INDEX `fk_celestial_body_has_comment_comment1_idx` (`comment_id` ASC),
-  INDEX `fk_celestial_body_has_comment_celestial_body1_idx` (`celestial_body_id` ASC),
-  CONSTRAINT `fk_celestial_body_has_comment_celestial_body1`
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comment_celestial_body1`
     FOREIGN KEY (`celestial_body_id`)
     REFERENCES `celestial_body` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_celestial_body_has_comment_comment1`
-    FOREIGN KEY (`comment_id`)
-    REFERENCES `comment` (`id`)
+  CONSTRAINT `fk_celestial_body_comment_celestial_body_comment1`
+    FOREIGN KEY (`in_reply_to_id`)
+    REFERENCES `celestial_body_comment` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `celestial_body_has_encounter`
+-- Table `location`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `celestial_body_has_encounter` ;
+DROP TABLE IF EXISTS `location` ;
 
-CREATE TABLE IF NOT EXISTS `celestial_body_has_encounter` (
+CREATE TABLE IF NOT EXISTS `location` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(250) NULL,
+  `address` VARCHAR(250) NULL,
+  `city` VARCHAR(45) NULL,
+  `state` VARCHAR(45) NULL,
+  `country` VARCHAR(45) NULL,
+  `zip_code` VARCHAR(25) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `encounter`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `encounter` ;
+
+CREATE TABLE IF NOT EXISTS `encounter` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(250) NOT NULL,
+  `description` TEXT NOT NULL,
+  `behavior` TEXT NOT NULL,
+  `enabled` TINYINT NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NULL,
+  `encounter_date` DATE NOT NULL,
+  `encounter_time` TIME NULL,
+  `capture_method` TEXT NULL,
+  `user_id` INT NOT NULL,
   `celestial_body_id` INT NOT NULL,
+  `location_id` INT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_Encounter_User1_idx` (`user_id` ASC),
+  INDEX `fk_encounter_celestial_body1_idx` (`celestial_body_id` ASC),
+  INDEX `fk_encounter_location1_idx` (`location_id` ASC),
+  CONSTRAINT `fk_Encounter_User1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_encounter_celestial_body1`
+    FOREIGN KEY (`celestial_body_id`)
+    REFERENCES `celestial_body` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_encounter_location1`
+    FOREIGN KEY (`location_id`)
+    REFERENCES `location` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `rating`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `rating` ;
+
+CREATE TABLE IF NOT EXISTS `rating` (
+  `user_id` INT NOT NULL,
   `encounter_id` INT NOT NULL,
-  PRIMARY KEY (`celestial_body_id`, `encounter_id`),
-  INDEX `fk_celestial_body_has_encounter_encounter1_idx` (`encounter_id` ASC),
-  INDEX `fk_celestial_body_has_encounter_celestial_body1_idx` (`celestial_body_id` ASC),
-  CONSTRAINT `fk_celestial_body_has_encounter_celestial_body1`
-    FOREIGN KEY (`celestial_body_id`)
-    REFERENCES `celestial_body` (`id`)
+  `rating_value` INT NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NULL,
+  INDEX `fk_rating_user1_idx` (`user_id` ASC),
+  PRIMARY KEY (`user_id`, `encounter_id`),
+  INDEX `fk_rating_encounter1_idx` (`encounter_id` ASC),
+  CONSTRAINT `fk_rating_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_celestial_body_has_encounter_encounter1`
+  CONSTRAINT `fk_rating_encounter1`
     FOREIGN KEY (`encounter_id`)
     REFERENCES `encounter` (`id`)
     ON DELETE NO ACTION
@@ -177,47 +221,81 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `star`
+-- Table `encounter_comment`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `star` ;
+DROP TABLE IF EXISTS `encounter_comment` ;
 
-CREATE TABLE IF NOT EXISTS `star` (
+CREATE TABLE IF NOT EXISTS `encounter_comment` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(250) NOT NULL,
-  `description` TEXT(20000) NULL,
-  `type` VARCHAR(45) NOT NULL,
+  `body` TEXT NOT NULL,
+  `enabled` TINYINT NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NULL,
+  `user_id` INT NOT NULL,
+  `encounter_id` INT NOT NULL,
+  `in_reply_to_id` INT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC))
+  INDEX `fk_Comment_User_idx` (`user_id` ASC),
+  INDEX `fk_encounter_comment_encounter1_idx` (`encounter_id` ASC),
+  INDEX `fk_encounter_comment_encounter_comment1_idx` (`in_reply_to_id` ASC),
+  CONSTRAINT `fk_Comment_User0`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_encounter_comment_encounter1`
+    FOREIGN KEY (`encounter_id`)
+    REFERENCES `encounter` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_encounter_comment_encounter_comment1`
+    FOREIGN KEY (`in_reply_to_id`)
+    REFERENCES `encounter_comment` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `solar_system`
+-- Table `encounter_image`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `solar_system` ;
+DROP TABLE IF EXISTS `encounter_image` ;
 
-CREATE TABLE IF NOT EXISTS `solar_system` (
+CREATE TABLE IF NOT EXISTS `encounter_image` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(250) NOT NULL,
-  `description` TEXT(20000) NULL,
-  `type` VARCHAR(45) NOT NULL,
+  `image_url` VARCHAR(2000) NOT NULL,
+  `encounter_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC))
+  INDEX `fk_encounter_image_encounter1_idx` (`encounter_id` ASC),
+  CONSTRAINT `fk_encounter_image_encounter1`
+    FOREIGN KEY (`encounter_id`)
+    REFERENCES `encounter` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `nebula`
+-- Table `user_favorited_encounter`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `nebula` ;
+DROP TABLE IF EXISTS `user_favorited_encounter` ;
 
-CREATE TABLE IF NOT EXISTS `nebula` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(250) NOT NULL,
-  `description` TEXT(20000) NULL,
-  `type` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC))
+CREATE TABLE IF NOT EXISTS `user_favorited_encounter` (
+  `user_id` INT NOT NULL,
+  `encounter_id` INT NOT NULL,
+  PRIMARY KEY (`user_id`, `encounter_id`),
+  INDEX `fk_user_has_encounter_encounter1_idx` (`encounter_id` ASC),
+  INDEX `fk_user_has_encounter_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_user_has_encounter_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_has_encounter_encounter1`
+    FOREIGN KEY (`encounter_id`)
+    REFERENCES `encounter` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 SET SQL_MODE = '';
@@ -236,7 +314,138 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `spacedb`;
-INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`) VALUES (1, 'admin', '1234', 1, 'ADMIN');
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `created_at`, `updated_at`, `role`, `image_url`, `about`) VALUES (1, 'admin', '1234', 1, '2023-05-18', '2023-05-19', 'ADMIN', NULL, NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `classification`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `spacedb`;
+INSERT INTO `classification` (`id`, `name`) VALUES (1, 'Nebula');
+INSERT INTO `classification` (`id`, `name`) VALUES (2, 'Solar System');
+INSERT INTO `classification` (`id`, `name`) VALUES (3, 'Star');
+INSERT INTO `classification` (`id`, `name`) VALUES (4, 'Planet');
+INSERT INTO `classification` (`id`, `name`) VALUES (5, 'Comet');
+INSERT INTO `classification` (`id`, `name`) VALUES (6, 'Asteroid');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `category`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `spacedb`;
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (1, 'Emission', 1);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (2, 'Planetary', 1);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (3, 'Supernova Remnant', 1);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (4, 'Dark', 1);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (5, 'Ordered', 2);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (6, 'Anti-Ordered', 2);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (7, 'Mixed', 2);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (8, 'Similar', 2);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (9, 'Neutron', 3);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (10, 'Red Dwarf', 3);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (11, 'Brown Drawf', 3);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (12, 'White Dwarf', 3);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (13, 'Yellow Dwarf', 3);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (14, 'Red Giant', 3);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (15, 'Proto', 3);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (16, 'Pulsar', 3);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (17, 'Rocky', 4);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (18, 'Gas', 4);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (19, 'Ice', 4);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (20, 'Minor', 4);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (21, 'Periodic', 5);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (22, 'Non-Periodic', 5);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (23, 'Non-Orbit', 5);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (24, 'Lost', 5);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (25, 'Carbon', 6);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (26, 'Metallic', 6);
+INSERT INTO `category` (`id`, `name`, `classification_id`) VALUES (27, 'Silicon', 6);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `celestial_body`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `spacedb`;
+INSERT INTO `celestial_body` (`id`, `name`, `created_at`, `enabled`, `updated_at`, `description`, `image_url`, `tracking_url`, `category_id`, `parent_celestial_body_id`) VALUES (1, 'Messier-16 (Eagle)', '2023-05-19', 1, NULL, 'open-cluster of stars in the constellation Serpens. Discovered in 1745.', 'https://cdn.spacetelescope.org/archives/images/screen/opo9544b.jpg', NULL, 1, NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `celestial_body_comment`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `spacedb`;
+INSERT INTO `celestial_body_comment` (`id`, `body`, `enabled`, `created_at`, `updated_at`, `user_id`, `celestial_body_id`, `in_reply_to_id`) VALUES (1, 'I hope you\'ll like this super neat picture I found!', 1, '2023-05-19', NULL, 1, 1, NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `location`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `spacedb`;
+INSERT INTO `location` (`id`, `name`, `address`, `city`, `state`, `country`, `zip_code`) VALUES (1, NULL, '123 Protect Yourself Way', 'Puhlease', 'NO', 'USA', '90210');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `encounter`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `spacedb`;
+INSERT INTO `encounter` (`id`, `title`, `description`, `behavior`, `enabled`, `created_at`, `updated_at`, `encounter_date`, `encounter_time`, `capture_method`, `user_id`, `celestial_body_id`, `location_id`) VALUES (1, 'Eagle Sighting!', 'It was a glowing and colorful night. Who could resist some telescope time? Lucky idea since I captured this boss image.', 'I caught it spitting out baby stars!', 1, '2023-05-19', NULL, '2023-05-18', NULL, 'QHY 5III585C Planetary Camera and Guider', 1, 1, NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `rating`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `spacedb`;
+INSERT INTO `rating` (`user_id`, `encounter_id`, `rating_value`, `created_at`, `updated_at`) VALUES (1, 1, 5, '2023-05-19', NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `encounter_comment`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `spacedb`;
+INSERT INTO `encounter_comment` (`id`, `body`, `enabled`, `created_at`, `updated_at`, `user_id`, `encounter_id`, `in_reply_to_id`) VALUES (1, 'Why hasn\'t anyone commented yet about how amazing my photo is?', 1, '2023-05-19', NULL, 1, 1, NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `encounter_image`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `spacedb`;
+INSERT INTO `encounter_image` (`id`, `image_url`, `encounter_id`) VALUES (1, 'http://cs.astronomy.com/cfs-file.ashx/__key/telligent-evolution-components-attachments/13-59-00-00-00-49-12-35/M16-_2D00_-SHO_2D00_RGB.jpg', 1);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `user_favorited_encounter`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `spacedb`;
+INSERT INTO `user_favorited_encounter` (`user_id`, `encounter_id`) VALUES (1, 1);
 
 COMMIT;
 
